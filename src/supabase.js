eba4c217079e9ab5900327cc3838fa008958ab03
zezo405060 <abdellazeezm@gmail.com>
2025-07-03 15:24:39 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://YOUR-PROJECT.supabase.co";
-const supabaseKey = "YOUR_PUBLIC_ANON_KEY";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://fdnocxrrkrwwocuesfgu.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
